bed8fee0a7b3f4b20a61cc610128c03ff2e445e2
Ian <ian.nptk@gmail.com>
2024-05-20 11:05:41 +0200

    completeled library

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const myLibrary = [];
+let myLibrary = [];
+const addBookButton = document.querySelector(".addBookButton");
+const addBookForm = document.querySelector(".addBookForm");
 
-function Book(title, author, pages, hasRead) {
+function Book(title, author, pages, hasRead, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.hasRead = hasRead;
+  this.id = id;
 }
+
+const libraryModule = {
+  nextId: 0,
+  addBookToLibrary: function (titleInput, authorInput, pagesInput, hasRead) {
+    const book = new Book(
+      titleInput,
+      authorInput,
+      pagesInput,
+      hasRead,
+      this.nextId
+    );
+    myLibrary.push(book);
+    this.nextId++;
+  },
+};
 
 Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${
@@ -13,35 +31,70 @@ Book.prototype.info = function () {
   }`;
 };
 
-function addBookToLibrary() {
-  let titleInput = prompt("Enter book title: ");
-  let authorInput = prompt("Enter book author: ");
-  let pagesInput = prompt("Enter number of pages: ");
-  let hasRead = confirm("Have you read this book yet?");
+Book.prototype.returnId = function () {
+  return this.id;
+};
 
-  const book = new Book(titleInput, authorInput, pagesInput, hasRead);
-  myLibrary.push(book);
-}
+Book.prototype.changeReadStatus = function () {
+  this.hasRead = !this.hasRead;
+  displayBooks();
+};
 
 function addBookFiller() {
-  const firstBook = new Book("Bliss", "Klsr", "100", true);
-  const secondBook = new Book("How To Fight", "Eartheater", "250", true);
-  const thirdBook = new Book("Somehow", "Tom Odell", "329", false);
-
-  myLibrary.push(firstBook);
-  myLibrary.push(secondBook);
-  myLibrary.push(thirdBook);
+  libraryModule.addBookToLibrary("Bliss", "Klsr", "100", true);
+  libraryModule.addBookToLibrary("How To Fight", "Eartheater", "250", true);
+  libraryModule.addBookToLibrary("Somehow", "Tom Odell", "329", false);
 }
 
 function displayBooks() {
   const library = document.querySelector(".library");
+  library.innerHTML = "";
+
   myLibrary.forEach(function (book) {
     const bookCard = document.createElement("div");
+    const removeBookButton = document.createElement("button");
+    const changeReadStatus = document.createElement("button");
+    const bookId = book.id;
+
     bookCard.classList.add("card");
+    bookCard.setAttribute("id", bookId);
     bookCard.innerHTML = book.info();
+    bookCard.innerHTML += `. ID: ${bookId}`;
+
+    changeReadStatus.addEventListener("click", () => {
+      book.changeReadStatus();
+    });
+    changeReadStatus.innerHTML = "Change read status";
+
+    removeBookButton.addEventListener("click", () => {
+      myLibrary = myLibrary.filter((item) => item.id !== bookId);
+      library.removeChild(bookCard);
+    });
+    removeBookButton.innerHTML = "Remove book";
+
+    bookCard.appendChild(changeReadStatus);
+    bookCard.appendChild(removeBookButton);
     library.appendChild(bookCard);
   });
 }
+
+addBookButton.addEventListener("click", () => {
+  addBookForm.classList.contains("hidden")
+    ? addBookForm.classList.remove("hidden")
+    : addBookForm.classList.add("hidden");
+
+  // displayBooks();
+});
+
+addBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleInput = addBookForm.title.value;
+  const authorInput = addBookForm.author.value;
+  const pagesInput = addBookForm.pages.value;
+  const hasRead = addBookForm.hasRead.checked;
+  libraryModule.addBookToLibrary(titleInput, authorInput, pagesInput, hasRead);
+  displayBooks();
+});
 
 //
 //
